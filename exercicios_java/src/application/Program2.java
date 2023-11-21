9f1eb02c92a00ab32331dfeed09c98f9f4435b6e
caue <caue.garbi@metareports.com.br>
2023-11-21 16:04:26 -0300

    New exercicies

---
+++ exercicios_java/src/application/Program2.java
@@ -0,0 +1,46 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		Product product = new Product();
+		System.out.println("Enter Product data: ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.print("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in stock: ");
+		product.quantity = sc.nextInt();
+		
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+		
+		System.out.println();
+		System.out.print("Enter the number of products to be removed from stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Updated data: " + product);
+
+		sc.close();
+	}
+
+}
---
+++ exercicios_java/src/entities/Triangle.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Triangle {
+	
+	public double a;
+	public double b;
+	public double c;
+	
+	
+	public double area() {
+		double p = (a + b + c) / 2.0;
+		return Math.sqrt(p * (p -a ) * (p -b) * (p -c));
+		
+	}
+
+}
+
