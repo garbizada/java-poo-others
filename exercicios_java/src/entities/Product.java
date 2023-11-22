86c1982ffb9aea1fc34d4f6bae2045d161aaa720
caue <caue.garbi@metareports.com.br>
2023-11-22 17:31:40 -0300

    Starting with Constructor in JAVA

--- exercicios_java/src/entities/Product.java
+++ exercicios_java/src/entities/Product.java
@@ -1,21 +1,47 @@
 package entities;
 
 public class Product {
-	
-	public String name;
-	public double price;
-	public int quantity;
-	
-	
+
+	private String name;
+	private double price;
+	private int quantity;
+
+	public Product(String name, double price, int quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
 	public void addProducts(int quantity) {
 		this.quantity += quantity;
 	}
-	
+
 	public void removeProducts(int quantity) {
 		this.quantity -= quantity;
 	}
-	
+
 	public String toString() {
-		return name + ", $ " + price + ", "+ quantity + " units, " + "Total: $ " + price * quantity; 
+		return name + ", $ " + price + ", " + quantity + " units, " + "Total: $ " + price * quantity;
 	}
 }
