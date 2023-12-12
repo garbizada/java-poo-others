eb7df7500e571bc10ccbd8e7676f1df33f62b79d
garbizada <caue.justen@hotmail.com>
2023-12-12 13:40:15 -0300

    Cleaner Code

--- exceptions_personalized/src/model/entities/Reservation.java
+++ exceptions_personalized/src/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation {
 	
 	private Integer roomNumber;
@@ -13,7 +15,10 @@ public class Reservation {
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 	
 	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut)throws DomainException {
+		if(!checkOut.after(checkIn)){
+			throw new DomainException ("Check-out date must be after check-in date!");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
@@ -49,22 +54,21 @@ public class Reservation {
 	}
 	
 	
-	public String updateDates(Date checkIn, Date checkOut) {
+	public void updateDates(Date checkIn, Date checkOut) throws DomainException{
 		
 		Date now = new Date();
 		
 		if(checkIn.before(now) || checkOut.before(now)) {
-			return "Reservation dates for updates must be future dates";
+			throw new DomainException ( "Reservation dates for updates must be future dates");
 		}
 		
 		if(!checkOut.after(checkIn)){
-			return "Check-out date must be after check-in date!";
+			throw new DomainException ("Check-out date must be after check-in date!");
 		}
 		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 		
-		return null;
 	}
 	
 	@Override
