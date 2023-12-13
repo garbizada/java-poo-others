7e76e29363e711dcea08945dc61fb1eed82bf66d
garbizada <caue.justen@hotmail.com>
2023-12-13 16:16:16 -0300

    Creating Board, pieces and game rules

--- chess_game/src/application/Program.java
+++ chess_game/src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch(); 
+		Ui.printBoard(chessMatch.getPieces());
 		
 	}
 }
