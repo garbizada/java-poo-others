819262d602729a411c226419c5ef70139fd24fb2
garbizada <caue.justen@hotmail.com>
2023-12-13 14:19:36 -0300

    Adding more classes

--- chess_game/src/application/Program.java
+++ chess_game/src/application/Program.java
@@ -1,5 +1,13 @@
 package application;
 
-public class Program {
+import boardgame.Board;
+import boardgame.Position;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		
+		Board board = new Board(8, 8);
+		
+	}
 }
