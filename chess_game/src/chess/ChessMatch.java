1a06574580c40a738f19229ab02f623c36122ded
garbizada <caue.justen@hotmail.com>
2023-12-14 15:53:04 -0300

    Throwing exceptions and some game rules

--- chess_game/src/chess/ChessMatch.java
+++ chess_game/src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.pieces.King;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -25,6 +26,7 @@ public class ChessMatch {
 	
 	private void initialSetup() {
 		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new King(board, Color.BLACK), new Position(2, 4));
 	}
 
 }
