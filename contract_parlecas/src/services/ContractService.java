41577b54cb9db2f5240577d35bcfcb1bdd17d256
garbizada <caue.justen@hotmail.com>
2024-02-07 16:35:03 -0300

    exercicio de contrato com parcelas

---
+++ contract_parlecas/src/services/ContractService.java
@@ -0,0 +1,27 @@
+package services;
+
+import java.time.LocalDate;
+
+import entities.Contract;
+import entities.Installment;
+
+public class ContractService {
+
+	private OnlinePaymentService onlinePaymentService;
+
+	public ContractService(OnlinePaymentService onlinePaymentService) {
+		this.onlinePaymentService = onlinePaymentService;
+	}
+	
+	
+	public void processContract(Contract contract, int months) {
+		
+		contract.getInstallments().add(new Installment(LocalDate.of(2018, 7, 25) ,206.04));
+		
+	}
+	
+	
+	
+	
+}
+ 
---
+++ contract_parlecas/src/services/OnlinePaymentService.java
@@ -0,0 +1,14 @@
+package services;
+
+public interface OnlinePaymentService {
+
+	double paymentFee(double amount);
+	
+	double interest(double amount, int months);
+	
+	
+	
+	
+	
+	
+}
