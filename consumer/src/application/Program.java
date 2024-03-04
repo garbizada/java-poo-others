e3c96da9e9a64fe2e5a56512b1e750c6cc9bbb3b
garbizada <caue.justen@hotmail.com>
2024-03-04 14:44:05 -0300

    Consumer forEach in JAVA

---
+++ consumer/src/application/Program.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("Hd Case", 80.90));
+		
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * factor);
+		};
+		
+		//list.forEach(Product::staticPriceUpdate);
+		//list.forEach(Product::noStaticPriceUpdate);
+		list.forEach(cons);
+		
+		list.forEach(System.out::println);
+		
+	}
+}
