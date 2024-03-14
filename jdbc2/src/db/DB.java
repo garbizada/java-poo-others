4e9e16739fba55aedf8a5ac36deee8ecd2206bd5
garbizada <caue.justen@hotmail.com>
2024-03-14 15:45:40 -0300

    Working with data, how to retrieve data from the database

--- jdbc2/src/db/DB.java
+++ jdbc2/src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -53,5 +55,35 @@ public class DB {
 		catch(IOException e ) {
 			throw new DbException(e.getMessage());		}
 	}
-
+	
+	
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+		
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		
+		
+	}
 }
+	
+		
+
+
+
+
